f69c81fa094b69cb1e792a3979be3cdd92835b29
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-11-03 13:37:06 -0500

    added home activities for each function

---
+++ app/src/main/java/com/example/cs315finalprojectekg/LearnColorsStart.java
@@ -0,0 +1,14 @@
+package com.example.cs315finalprojectekg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnColorsStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_colors_start);
+    }
+}
---
+++ app/src/main/java/com/example/cs315finalprojectekg/LearnLettersStart.java
@@ -0,0 +1,14 @@
+package com.example.cs315finalprojectekg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnLettersStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_letters_start);
+    }
+}

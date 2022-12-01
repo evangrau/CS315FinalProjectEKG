0d8413874e4377f9d95871941cbcdacaf10ede12
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-12-01 08:16:56 -0600

    start activities go to quiz activity

--- app/src/main/java/com/example/cs315finalprojectekg/LearnColorsStart.java
+++ app/src/main/java/com/example/cs315finalprojectekg/LearnColorsStart.java
@@ -2,13 +2,27 @@ package com.example.cs315finalprojectekg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LearnColorsStart extends AppCompatActivity {
+
+    Button startQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_colors_start);
+
+        startQuiz = (Button) findViewById(R.id.start_colors);
+        startQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LearnColorsStart.this, QuizActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cs315finalprojectekg/LearnLettersStart.java
+++ app/src/main/java/com/example/cs315finalprojectekg/LearnLettersStart.java
@@ -2,13 +2,27 @@ package com.example.cs315finalprojectekg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LearnLettersStart extends AppCompatActivity {
+
+    Button startQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_letters_start);
+
+        startQuiz = (Button) findViewById(R.id.start_letters);
+        startQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LearnLettersStart.this, QuizActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

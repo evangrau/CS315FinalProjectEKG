cc3e664ad65dd2b1553375efb35c51fb8ac1f873
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-12-01 08:45:49 -0600

    added quiz buttons and test implementation

--- app/src/main/java/com/example/cs315finalprojectekg/QuizActivity.java
+++ app/src/main/java/com/example/cs315finalprojectekg/QuizActivity.java
@@ -2,13 +2,56 @@ package com.example.cs315finalprojectekg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    Button mainMenuBtn;
+    Button redBtn, blueBtn, yellowBtn, greenBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mainMenuBtn = (Button) findViewById(R.id.main_menu_btn);
+        mainMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuizActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        redBtn = (Button) findViewById(R.id.top_left);
+        redBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("This is the red button");
+            }
+        });
+        blueBtn = (Button) findViewById(R.id.top_right);
+        blueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("This is the blue button");
+            }
+        });
+        yellowBtn = (Button) findViewById(R.id.bottom_left);
+        yellowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("This is the yellow button");
+            }
+        });
+        greenBtn = (Button) findViewById(R.id.bottom_right);
+        greenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("This is the green button");
+            }
+        });
     }
 }

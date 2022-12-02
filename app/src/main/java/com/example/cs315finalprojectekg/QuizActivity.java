313f729a817172b3c54ee996ad05896110c710c7
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-12-01 22:51:28 -0600

    show answer button implementation

--- app/src/main/java/com/example/cs315finalprojectekg/QuizActivity.java
+++ app/src/main/java/com/example/cs315finalprojectekg/QuizActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ public class QuizActivity extends AppCompatActivity {
     Intent intent;
     Bundle extras;
     Button mainMenuBtn;
+    Button showAnswerBtn;
     Button topLeftBtn, topRightBtn, bottomLeftBtn, bottomRightBtn;
     TextView pointsLabel, answerLabel;
     int points = 0;
@@ -30,27 +32,37 @@ public class QuizActivity extends AppCompatActivity {
 
         pointsLabel = (TextView) findViewById(R.id.points_label);
         answerLabel = (TextView) findViewById(R.id.answer_label);
+        answerLabel.setVisibility(View.INVISIBLE);
+
+        showAnswerBtn = (Button) findViewById(R.id.show_answer);
+        showAnswerBtn.setOnClickListener(view -> {
+            answerLabel.setVisibility(View.VISIBLE);
+        });
 
         mainMenuBtn = (Button) findViewById(R.id.main_menu_btn);
         mainMenuBtn.setOnClickListener(view -> {
             Intent intent = new Intent(QuizActivity.this, MainActivity.class);
             startActivity(intent);
         });
+
         topLeftBtn = (Button) findViewById(R.id.top_left);
         topLeftBtn.setOnClickListener(view -> {
             System.out.println("This is the top left button");
             changePoints(topLeftBtn.getText().toString());
         });
+
         topRightBtn = (Button) findViewById(R.id.top_right);
         topRightBtn.setOnClickListener(view -> {
             System.out.println("This is the top right button");
             changePoints(topRightBtn.getText().toString());
         });
+
         bottomLeftBtn = (Button) findViewById(R.id.bottom_left);
         bottomLeftBtn.setOnClickListener(view -> {
             System.out.println("This is the bottom left button");
             changePoints(bottomLeftBtn.getText().toString());
         });
+
         bottomRightBtn = (Button) findViewById(R.id.bottom_right);
         bottomRightBtn.setOnClickListener(view -> {
             System.out.println("This is the bottom right button");
@@ -70,6 +82,7 @@ public class QuizActivity extends AppCompatActivity {
             points++;
         }
         pointsLabel.setText("Points: " + points);
+        answerLabel.setVisibility(View.INVISIBLE);
         if (quizType.equals("colors")) {
             selectColor();
         } else {

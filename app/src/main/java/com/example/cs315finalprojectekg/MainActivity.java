808298e8e57d2adf32cd4c9b324451d1bf158e33
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-12-02 19:16:39 -0600

    removed coloring book

--- app/src/main/java/com/example/cs315finalprojectekg/MainActivity.java
+++ app/src/main/java/com/example/cs315finalprojectekg/MainActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button lettersBtn, colorsBtn, coloringBookBtn;
+    Button lettersBtn, colorsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +24,6 @@ public class MainActivity extends AppCompatActivity {
         colorsBtn = (Button) findViewById(R.id.learn_colors_btn);
         colorsBtn.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, LearnColorsStart.class);
-            startActivity(intent);
-        });
-
-        coloringBookBtn = (Button) findViewById(R.id.coloring_book_btn);
-        coloringBookBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, ColoringBookHome.class);
             startActivity(intent);
         });
     }
